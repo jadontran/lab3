eb455c3ba7bf9a30d40805f6da5947b3a72660be
jadontran <114566278+jadontran@users.noreply.github.com>
2022-10-14 22:45:46 -0700

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -1,5 +1,19 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
 
 public class ListTests {
+    
+    @Test
+    public void testFilter(){
+        StringChecker sc = new Checker();
+        ArrayList<String> input = new ArrayList<>();
+        input.add(0, "1");
+        input.add(1, "2");
+        input.add(2, "3");
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add(0, "2");
+        expected.add(1, "3");
+        assertEquals(expected, ListExamples.filter(input, sc));
+    }
 }

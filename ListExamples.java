23d563fee4b2b77ea3dabb09f06cd607b0cc1cc2
jadontran <114566278+jadontran@users.noreply.github.com>
2022-10-14 22:45:44 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -3,6 +3,15 @@ import java.util.List;
 
 interface StringChecker { boolean checkString(String s); }
 
+class Checker implements StringChecker {
+  public boolean checkString(String s) {
+    if (s.contains("1")) {
+      return true;
+    }
+     return false;
+  }
+}
+
 class ListExamples {
 
   // Returns a new list that has all the elements of the input list for which
